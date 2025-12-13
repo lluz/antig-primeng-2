06563d3948524cff35f63c96af8278b808966f39
lluz <luisluz@gmail.com>
2025-12-13 13:08:02 +0000

    tailwindcss v4 (not displaying styles)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { Shell } from './shell/shell';
 import { Home } from './pages/home/home';
 import { Page2 } from './pages/page2/page2';
+import { Page3 } from './pages/page3/page3';
 
 export const routes: Routes = [
     {
@@ -9,7 +10,8 @@ export const routes: Routes = [
         component: Shell,
         children: [
             { path: '', component: Home },
-            { path: 'page2', component: Page2 }
+            { path: 'page2', component: Page2 },
+            { path: 'page3', component: Page3 }
         ]
     }
 ];
