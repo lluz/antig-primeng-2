0a8345411e33501d257dc8569d59e2701626eac1
lluz <luisluz@gmail.com>
2025-12-13 14:15:59 +0000

    feat: add new Page3 component with PrimeNG button and integrate Tailwind CSS for global styling (overrides).

--- src/app/pages/page3/page3.ts
+++ src/app/pages/page3/page3.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-page3',
-  imports: [],
+  imports: [ButtonModule, RouterLink],
   templateUrl: './page3.html',
   styleUrl: './page3.css',
 })
